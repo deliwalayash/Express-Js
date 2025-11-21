c620a8f6b93d420f9cc6f7c1d42cc3662026edaa
deliwalayash <deliwalayash2@gmail.com>
2025-11-21 11:51:40 +0530

    crud

--- routes-4/index.js
+++ routes-4/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const PORT=9000
 const app=express()
 const router=require('./routes/blogRoutes.js')
-
+app.use(express.urlencoded({extended:true}))
 app.use(router)
 
 app.listen(PORT,()=>{
--- routes-4/routes/blogRoutes.js
+++ routes-4/routes/blogRoutes.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const router=express.Router()
-const users=require('./blogs.js')
+let users=require('./blogs.js')
 const path=require('path')
+let upDateusers=users
 
 
 router.get('/',(req,res)=>{
@@ -10,6 +11,47 @@ router.get('/',(req,res)=>{
 
 router.get('/data',(req,res)=>{
     res.sendFile(path.join(__dirname,'./blogs.js'))
+})
+router.get('/delete/:id',(req,res)=>{
+    const deleteid=req.params.id
+    upDateusers=upDateusers.filter(i => i.id != Number(deleteid))
+    res.render('index.ejs',{users:upDateusers})
+})
+router.get('/form',(req,res)=>{
+    res.render('form.ejs')
+})
+router.get('/edit/:id',(req,res)=>{
+    const editUserid=Number(req.params.id)
+    const edituser=users.find(i => i.id ==editUserid)
+    res.render('edit.ejs',{user:edituser})
+
+})
+router.post('/edit/:id',(req,res)=>{
+    const editUserid=Number(req.params.id)
+    const index=users.findIndex( i => i.id == editUserid)
+    users[index].first_name=req.body.first_name
+    users[index].last_name=req.body.last_name
+    users[index].email=req.body.email
+    res.redirect("/")
+})
+
+
+
+router.post('/form',(req,res)=>{
+    const newUser={
+        id:users.length+1,
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        email:req.body.email
+    }
+    users.push(newUser)
+    res.redirect('/')
+})
+
+router.get('/showdata/:id',(req,res)=>{
+    const showuserid=Number(req.params.id)
+    const showuser=users.find(i => i.id ==showuserid)
+    res.render('users.ejs',{user:showuser})
 })
 router.get('/data/:id',(req,res)=>{
     const id=req.params.id
