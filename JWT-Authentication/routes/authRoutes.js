9723c5bfce9a4722afc92181572a07862007574a
deliwalayash <deliwalayash2@gmail.com>
2025-12-04 19:39:28 +0530

    login done

--- JWT-Authentication/routes/authRoutes.js
+++ JWT-Authentication/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router= express.Router()
 const User = require('../models/User')
 const bcrypt=require('bcrypt')
-
+const jwt=require('jsonwebtoken')
 
 router.post('/signup',async(req,res)=>{
     try{
@@ -33,6 +33,44 @@ router.post('/signup',async(req,res)=>{
             success:false,
             message:err.message
         })
+
+    }
+})
+
+router.post('/login',async(req,res)=>{
+
+    try{
+        const {email,password}=req.body
+
+        const founduser = await User.findOne({email})
+        if(!founduser){
+            return res.status(400).json({
+                success:false,
+                message:"User Not Exist"
+            })
+        }
+
+        const isMatch=await bcrypt.compare(password,founduser.password)
+        if(!isMatch){
+            return res.status(400).json({
+                success:false,
+                message:"Password Do not Match"
+            })
+
+        }
+        const token=jwt.sign(
+            {userId:founduser._id,role:founduser.role},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        )
+
+        return res.status(200).json({
+        success: true,
+        message: "Login Successful",
+        token: token
+        })
+
+    }catch(err){
 
     }
 })
